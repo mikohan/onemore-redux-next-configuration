ad9280e54f5cdd43604df8a94c2173ec7069ec66
manhee <manhee@localhost.localdomain>
2020-12-01 14:52:26 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>My next application for using redux tests</h1>
+
+    </div>
+  );
+}
