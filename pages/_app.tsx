2de138c260bfa2e49f074aeb88cb1d3ab56c7c61
mikohan <angara99@gmail.com>
2020-12-01 18:41:48 +0300

    Needs to be changed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+//   \_app.js
+import App, { Container } from "next/app";
+import React from "react";
+import withReduxStore from "../src/lib/with-redux-store";
+import { Provider } from "react-redux";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, reduxStore } = this.props;
+    return (
+      <Container>
+        <Provider store={reduxStore}>
+          <Component {...pageProps} />
+        </Provider>
+      </Container>
+    );
+  }
 }
 
-export default MyApp
+export default withReduxStore(MyApp);
---
+++ src/redux/store.ts
@@ -0,0 +1,13 @@
+//  /src/store.js
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { rootReducer } from "./reducers/root";
+
+export function initializeStore(initialState = {}) {
+  return createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+}
